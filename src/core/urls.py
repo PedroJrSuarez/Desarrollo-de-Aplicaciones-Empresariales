08876e7a37f907da23b73151b722568e8eda85d9
PedroJrSuarez <pedrojsuarez3005@gmail.com>
2026-09-04 23:54:38 -0500

    Proyecto Django Semana 03 Update 2.0

--- src/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import item_api, item_list
-
-urlpatterns = [
-    path('api/items/', item_api, name='item_api'),
-    path('', item_list, name='item_list'),
-]
